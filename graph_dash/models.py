e1f39d5602594bfedd2d4c931ee9094a620128e5
kaushik <kaushin@bgsu.edu>
2018-03-15 15:42:35 -0400

    line graph

--- graph_dash/models.py
+++ graph_dash/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TestResult(models.Model):
+    date = models.DateField()
+    application = models.CharField(max_length=50)
+    result = models.BooleanField()
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.date,self.application) 
--- graph_dash/views.py
+++ graph_dash/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import TestResult
+
 # Create your views here.
 def index(request):
-    return render(request, 'graph_dash/index.html', context={'data':'dsfpvkispdkip' })
+    results = TestResult.objects.all()
+    nums = [10,11,12,13,14,15]
+    return render(request, 'graph_dash/index.html', context={'data':results , 'line_data':nums })
