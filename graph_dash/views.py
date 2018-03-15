6ca6c0868190f503fda3a1594dd9207a7fea0b0c
kaushik <kaushin@bgsu.edu>
2018-03-15 15:14:16 -0400

    dash template

--- graph_dash/views.py
+++ graph_dash/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', context={'data':'dsfpvkispdkip' })
+    return render(request, 'graph_dash/index.html', context={'data':'dsfpvkispdkip' })
